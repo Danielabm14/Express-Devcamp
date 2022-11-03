1545a9c0f3a24c9b942de51842cebd8a8f9c027f
Your Name <you@example.com>
2022-11-03 11:14:31 -0500

    validaciones

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -2,7 +2,8 @@
 //el objeto de conexion
 const Sequelize = require('../config/seq')
 //datatypes de sequelize
-const { DataTypes } = require('sequelize')
+const { DataTypes, ValidationError } = require('sequelize')
+
 //el modelo
 const UserModel = require('../models/user')
 
@@ -15,18 +16,43 @@ const User = UserModel(Sequelize,DataTypes)
 //crear nuevo user
 exports.CreateUser = async (req, res) => {
     //crear los usuarios
-    const newUser = await User.create(req.body)
-    res
-        .status(200)
-        .json({
+    try {
+        const newUser = await User.create(req.body)
+     
+            res
+            .status(200)
+            .json({
             "succes" : true,
             "data" : newUser
-        })
+            })
+    } catch (error) {
+        if(error instanceof ValidationError){
+            //recorrer arreglo de errores
+            //foreach
+            //map
+            const errores = error.errors.map((elemento) => elemento.message)
+            res
+            .status(400)
+            .json({
+                "succes": false,
+                "errors": error
+                 })
+        }else{
+            res
+            .status(400)
+            .json({
+                "succes": false,
+                "errors": "error de servidor"
+                 })
+        }
+    }
+   
 }
 
 
-//listar todos los user
+//traer todos los user
 exports.getAllUsers = async (req, res) => {
+try {
     //traer los usuarios
     const users = await User.findAll();
     res
@@ -35,13 +61,24 @@ exports.getAllUsers = async (req, res) => {
             "succes" : true,
             "data" : users
         })
+} catch (error) {
+    res
+    .status(400)
+    .json({
+        "succes": false,
+        "errors": "error,servidor desconocido"
+         })
+}
+
+    
 }
 
 
 //listar user por id 
 exports.getSingleUser = async (req, res) => {
+try {
     const singleUser = await User.findByPk(req.params.id);
-    console.log(req.params.id)
+    if(singleUser){
     res
         .status(200)
         .json({
@@ -49,40 +86,100 @@ exports.getSingleUser = async (req, res) => {
             "data" : singleUser
         })
 
-    }
-//actualizar user
-exports.updateUser = async (req, res)=>{
-    // Change everyone without a last name to "Doe"
-    await User.update( req.body, {
-        where: {
-            id: req.params.id
-        }
-    });
-    const singleUser = await User.findByPk(req.params.id)
-    //console.log(req.params.id)
+}else{
     res
-        .status(200)
-        .json({
-           "success": true,
-           "data": singleUser
-    })
+    .status(00)
+    .json({
+        "succes": false,
+        "errors": "usuario no existente"
+        })
+        }
+} catch (error) {
+    res
+    .status(400)
+    .json({
+        "succes": false,
+        "errors": "error servidor desconocido"
+         })
 }
+
+    
+
+    }
+//Actualizar el user 
+exports.updateUser = async (req, res)=>{
+    try {
+     const singleUser = await User.findByPk(req.params.id);
+     if(!singleUser){
+         res
+         .status(400)
+         .json({
+             "success": false,
+             "errors": "usuario no existe"
+         })
+     }else{
+         // actualizar usuario
+     await User.update(req.body, {
+         where: {
+           id: req.params.id
+         } });
+         //selecciono user actualizado
+         const updatedUser = await User.findByPk(req.params.id)
+       //envia respuesta
+       res
+       .status(200)
+       .json({
+           "success": true,
+           "data": updatedUser
+       }) 
+     }
+      
+    } catch (error) {
+     res
+         .status(400)
+         .json({
+             "success": false,
+             "errors": "error de servidor desconocido"
+         })
+    }
+ }
 
 
 //Eliminar user
 
-exports.deleteUser = async (req, res) => {
-    const singleUser = await User.findByPk(req.params.id)
-    await User.destroy({
-        where: {
-          id: req.params.id
-        }
-      });
-    
-    res
-        .status(200)
-        .json({
-            "succes" : true,
-            "data" : singleUser
+exports.deleteUser = async (req, res)=>{
+    //console.log(req.params.id)
+    try {
+        const SingleUser = await User.findByPk(req.params.id);
+        if (!SingleUser) {
+            res
+            .status(400)
+            .json({
+                "success": false,
+                "errors": "Usuario no existente"
         })
+        } else {
+            await User.destroy({
+                where: {
+                    id: req.params.id
+                }
+              });
+        const deleteUser = await User.findByPk(req.params.id);
+
+            res
+            .status(200)
+            .json({
+                "success": true,
+                "data": SingleUser
+            })
+        }
+    } catch (error) {
+        res
+        .status(400)
+        .json({
+            "success": false,
+            "errors": " Error de servidor desconocido"
+        })
+    }
+    
 }
--- models/user.js
+++ models/user.js
@@ -2,6 +2,7 @@
 const {
   Model
 } = require('sequelize');
+const { DataTypes, UniqueConstraintError } = require('sequelize')
 module.exports = (sequelize, DataTypes) => {
   class User extends Model {
     /**
@@ -14,13 +15,60 @@ module.exports = (sequelize, DataTypes) => {
     }
   }
   User.init({
-    username: DataTypes.STRING,
-    email: DataTypes.STRING,
-    password: DataTypes.STRING
-  }, {
+        username: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        UniqueConstraintError: {
+          args: true,
+          msg: 'already exists'
+      },
+        validate:{
+          unique(value) {
+          
+            return User.findOne({where:{username:value}})
+              .then((username) => {
+                if (username) {
+                  throw new Error('Error hay mas de un nombre asi');
+                }
+              })
+          },
+        isAlpha: {
+          args: true,
+          msg: "username debe tener solo letras "
+        },
+        notNull: {
+          args: true,
+          msg: "username debe estar presente "
+        },
+        notEmpty: {
+          args: true,
+          msg: "username no debe ser vacio "
+        },
+        
+      }
+     },
+     email:{
+      type:DataTypes.STRING,
+      validate: {
+        isEmail:{
+        args: true,
+        msg: "email no valido"
+      },
+    },
+  },
+     password:{
+      type:DataTypes.STRING,
+      validate:{
+        len: {
+          args: [5,10],
+          msg: "password minimo 5 y maximo 10 caracteres"
+        }
+      },
+     },
+    },{ 
     sequelize,
     modelName: 'User',
     timestamps: false
-  });
+     });
   return User;
 };
